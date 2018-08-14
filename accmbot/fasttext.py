e0388eecff3d1a081660382fe0797455961694cd
nikanar <nikana@sancta-terra.com>
2018-08-14 09:37:41 -0400

    flattened code much, + TODO

--- accmbot/fasttext.py
+++ accmbot/fasttext.py
@@ -9,33 +9,38 @@ class FasttextHandler(AccmHandler):
         self.response_channel = response_channel
 
     def handle_message(self, slack_client, event):
-        if event["channel"] == self.listen_channel:
-            message = event["text"].strip()
-            BOT = "bot"
-            if message.startswith(BOT):
-                msg = message[len(BOT):].strip()
-                response = self.get_label(msg)
-                if response:
-                    print("selftext text: {} response: {}".format(msg, response))
-                    slack_client.api_call("chat.postMessage", channel=self.response_channel, text=response)
-                else:
-                    print("no response")
+        if event["channel"] != self.listen_channel: return
+        message = event["text"].strip()
+        BOT_TRIGGER = "bot"
+        if not message.startswith(BOT_TRIGGER): return
+        msg = message[len(BOT_TRIGGER):].strip()
+        
+        label = self.get_label(msg)
+        prettyfy = {b"__label__u" : "This is [URGENT] !!",
+                    b"__label__d" : "Spell out an answer",
+                    b"__label__a" : "This is an administrative query, we have a precanned answer to it.",
+                    b"__label__r" : "Ignore this message."
+                    }
+        # TODO Spell the administrative options out to the user somehow
+        # they currently live in test.json.
+        try:
+            response = prettyfy[label]
+        except KeyError:
+            print("no response or unexpected response")
+        else:
+            print("fasttext answers '{}' with '{}'".format(msg, response))
+            slack_client.api_call("chat.postMessage", channel=self.response_channel, text=response)
+
 
     def get_label(self, msg):
         fasttext_cmd = "/Users/jp/Desktop/fastText-0.1.0/fasttext"
         model_file = "/Users/jp/Desktop/fastText-0.1.0/model.bin"
-        with open('/Users/jp/Desktop/fastText-0.1.0/tmp', 'w') as fout:
-            fout.write(msg)
-            fout.write('\n')
+        with open('tmp', 'w') as tmp:
+            tmp.write(msg)
+            tmp.write('\n')
 
-        process = Popen([fasttext_cmd, 'predict', model_file, '/Users/jp/Desktop/fastText-0.1.0/tmp'], stdout=PIPE, stderr=PIPE)
+        process = Popen([fasttext_cmd, 'predict', model_file, 'tmp'], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
         stdout = stdout.strip()
-        prettyfy = {b"__label__d" : "Spell out an answer",
-                    b"__label__a" : "Give <THIS> precanned answer to this 'administrative' query.",
-                     b"__label__r" : "Ignore this message.",
-                    b"__label__u" : "This is [URGENT] !!"
-        }
         print("stdout: {}".format(stdout))
-        if stdout in prettyfy:
-            return prettyfy[stdout]
+        return stdout
