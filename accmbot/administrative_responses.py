661fabef5f81a885682b3c80798172e29307bdfd
nikanar <nikana@sancta-terra.com>
2018-08-14 10:11:11 -0400

    Display all administrative answers, next pick the correct one

---
+++ accmbot/administrative_responses.py
@@ -0,0 +1,24 @@
+from accmbot import AccmHandler
+
+class Autoanswerer(AccmHandler):
+    def __init__(self, listen_channel, response_channel):
+        self.listen_channel = listen_channel
+        self.response_channel = response_channel
+
+    def handle_message(self, slack_client, event):
+        """Move the 'This is an administrative query, we have a precanned answer to it.' ball forward with the correct answer."""
+
+        if event["channel"] != self.listen_channel: return
+        message = event["text"].strip()
+        BOT_TRIGGER = "This is an administrative query"
+        if not BOT_TRIGGER in message: return
+        msg = "FIXME" # get this from the FasttextHandler, or parse the conversation above.
+
+        json_data = json.load(open("text.json"))
+        precanned_answers = [e['response'] for e in json_data if "test" not in e['response']]
+        response = "\n\n".join(precanned_answers)
+        # FIXME pick the correct answer to the msg instead of sending them all
+        
+        print("bot selects {} from the list of preformatted answers"
+              .format("FIXME-currently-doesn't-select"))
+        slack_client.api_call("chat.postMessage", channel=self.response_channel, text=response)
--- accmbot/start_bot.py
+++ accmbot/start_bot.py
@@ -13,8 +13,9 @@ def main():
     helpdesk_handler = HelpdeshHandler("CC68GD2QG", "CC73Z93E1")
     keyword_handler = KeywordHandler(JsonKeywordData("./test.json"), "CC68GD2QG")
     fasttext_handler = FasttextHandler("CC68GD2QG", "CC73Z93E1")
+    autoanswer_handler = Autoanswerer("CC68GD2QG", "CC73Z93E1")
 
-    accm_bot = AccmBot(token, keyword_handler, helpdesk_handler, fasttext_handler)
+    accm_bot = AccmBot(token, keyword_handler, helpdesk_handler, fasttext_handler, )
     accm_bot.add_channels(*LISTEN_CHANNELS)
     accm_bot.connect()
     accm_bot.start()
