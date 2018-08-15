813c67e0285435cae82ec7fdd97f3a2c01f7484f
nikanar <nikana@sancta-terra.com>
2018-08-15 17:50:34 -0400

    added a discreet mode and now it listens to two channels

--- accmbot/fasttext.py
+++ accmbot/fasttext.py
@@ -4,22 +4,21 @@ from accmbot import AccmHandler
 
 
 class FasttextHandler(AccmHandler):
-    def __init__(self, listen_channel):
-        self.listen_channel = listen_channel
-        self.response_channel = listen_channel
+    def __init__(self, listen_channels, discreet_channel):
+        self.listen_channels = listen_channels
+        self.discreet_channel = discreet_channel
         self.BOT_TRIGGER = "bot"
-
         
     def handle_message(self, slack_client, event):
-        if event["channel"] != self.listen_channel: return
+        if event["channel"] not in self.listen_channels: return
         message = event["text"].strip()
         if not message.startswith(self.BOT_TRIGGER): return
         msg = message[len(self.BOT_TRIGGER):].strip()
         
         label = self.get_label(msg)
-        prettyfy = {b"__label__u" : "This is [URGENT] !!",
+        prettyfy = {b"__label__u" : "This seems urgent.",
                     b"__label__d" : "Spell out an answer",
-                    b"__label__a" : "This is an administrative query, we have a precanned answer to it.",
+                    b"__label__a" : "This is a usual query, we already have an answer to it. Let me find it...",
                     b"__label__r" : "Ignore this message."
                     }
         try:
@@ -27,8 +26,10 @@ class FasttextHandler(AccmHandler):
         except KeyError:
             print("no response or unexpected response")
         else:
-            print("fasttext answers '{}' with '{}'".format(msg, response))
-            slack_client.api_call("chat.postMessage", channel=self.response_channel, text=response)
+            if (event["channel"] != self.discreet_channel
+                or label in [b"__label__u", b"__label__a"]):
+                print("fasttext answers '{}' with '{}'".format(msg, response))
+                slack_client.api_call("chat.postMessage", channel=event["channel"], text=response)
 
 
     def get_label(self, msg):
--- accmbot/start_bot.py
+++ accmbot/start_bot.py
@@ -9,13 +9,15 @@ from administrative_responses import Autoanswerer
 TOKEN = "xoxb-416305909766-414764731412-jaXu2M1NO9khA3bJvBjVFT6Z"
 LISTEN_CHANNELS = ["CC68GD2QG", "CC82B8Z6K"]
 
+# phaccm2018.slack.com
+CHANNEL_IDS = {"#general" : "CC6R5LSHZ", "#bottest": "CC68GD2QG", "#fasttext" : "CC82B8Z6K"}
 
 def main():
     token = TOKEN or os.environ.get('SLACK_BOT_TOKEN')
     helpdesk_handler = HelpdeshHandler("CC68GD2QG", "CC73Z93E1")
-    keyword_handler = KeywordHandler(JsonKeywordData("./administration.json"), "CC68GD2QG")
-    fasttext_handler = FasttextHandler("CC68GD2QG")
-    autoanswer_handler = Autoanswerer("CC68GD2QG")
+    keyword_handler = KeywordHandler(JsonKeywordData("./administration.json"), CHANNEL_IDS["#bottest"])
+    fasttext_handler = FasttextHandler([CHANNEL_IDS["#bottest"], CHANNEL_IDS["#fasttext"]], CHANNEL_IDS["#fasttext"])
+    autoanswer_handler = Autoanswerer(CHANNEL_IDS["#bottest"])
 
     accm_bot = AccmBot(token, keyword_handler, helpdesk_handler, fasttext_handler, autoanswer_handler)
     accm_bot.add_channels(*LISTEN_CHANNELS)
